5585399172ddc53c5502f6f33a45ca790d4bc891
Hjeandedieu <hashimweyesujeandedie@gmail.com>
2026-06-11 20:03:59 +0200

    All routes scaffolded

---
+++ backend/src/routes/chat.routes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const chatsRouter = Router();
+
+export default chatsRouter;
---
+++ backend/src/routes/memories.routes.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const memoriesRouter = Router();
+
+export default memoriesRouter;
